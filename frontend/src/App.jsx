14471bfe3ad61d47acc401532c005e51371e85b5
Hassan <syedhassan3038@gmail.com>
2025-01-17 08:17:20 +0500

    create login and register page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,18 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
+import Login from './pages/Login'
+import Register from "./pages/Register";
 
 const App = () => {
   return (
-    <>
+    <div className="bg-blue-950 min-h-screen min-w-screen">
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/login" element={<div>Login</div>} />
-        <Route path="/register" element={<div>Register</div>} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
       </Routes>
-    </>
+    </div>
   );
 };
 
---
+++ frontend/src/pages/Login.jsx
@@ -0,0 +1,64 @@
+import React from "react";
+import { Link, useNavigate } from "react-router-dom";
+import axios from "../config/axios";
+import { useState } from "react";
+
+const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
+
+  const submitHandler = e => {
+    e.preventDefault();
+    axios.post(
+      "/login",
+      (email, password).then((res) => {
+        console.log(res.data);
+        navigate("/");
+      }).catch((err) => {
+        console.log(err);
+      })
+    );
+  };
+  return (
+    <div className="h-screen w-screen flex  justify-center items-center">
+      <div className="pb-16 text- flex flex-col justify-center items-center">
+        <div className=" bg-white px-10 py-14  rounded-lg shadow-md  m-auto cursor-pointer ">
+          <h1 className="text-4xl font-semibold text-neutral-800 pt-2 mb-10 md:-16">
+            Login
+          </h1>
+          <form onSubmit={submitHandler} className="flex flex-col gap-3  min-w-[22vw]">
+            <label htmlFor="">Email</label>
+            <input
+            onChange={e=>setEmail(e.target.value)}
+            value={email}
+              type="text"
+              placeholder="Email"
+              className="bg-zinc-100  rounded-md p-4 "
+            />
+            <label htmlFor="">Password</label>
+            <input
+            onChange={e=>setPassword(e.target.value)}
+            value={password}
+              type="password"
+              placeholder="Password"
+              className="bg-zinc-100 rounded-md p-4 "
+            />
+
+            <button className="font-semibold bg-black text-white mt-4 p-3 text-center rounded-full">
+              Continue
+            </button>
+          </form>
+          <p className="mt-5">
+            Dont have Account?{" "}
+            <Link to="/register" className="text-blue-400">
+              register
+            </Link>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
