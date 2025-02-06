254b49113b63faefa1cccf64c8dce2ad8f9ba49a
Hassan <syedhassan3038@gmail.com>
2025-02-06 16:32:05 +0500

    Create a new project modal

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,42 +1,60 @@
 import React, { useContext, useState } from "react";
 import { UserContext } from "../context/UserContext";
-import upload from "../assets/uplaod.svg";
+import add from "../assets/add.svg";
 
 const Home = () => {
   const { user } = useContext(UserContext);
+  const [isModal, setIsModal] = useState(false);
+  const createProject = (e)=>{
+    e.preventDefault()
+    console.log('Project created !')
+  }
 
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-[#1a1a1a] text-neutral-300">
-      <div className="w-full max-w-2xl bg-[#2F2F2F] rounded-lg shadow-xl overflow-hidden">
-        
-        <header className="bg-[#212121] text-2xl text-center p-6">
-          <h1>Welcome to CodeBot</h1>
-        </header>
-        
-        <div className="p-4 h-96 overflow-y-auto">
-          <div className="space-y-4">
-            <div className={`flex `}>
-              <div className={`max-w-xs p-3 rounded-lg `}>
-                <p>---</p>
-              </div>
+    <main className="">
+      {isModal && (
+        <div className="h-screen w-screen backdrop-blur-lg absolute flex justify-center items-center">
+          <div className=" modal bg-white p-4 rounded-md w-[350px] flex flex-col gap-4 min-h-[180px]">
+            <h1 className="font-semibold text-2xl text-zinc-900">
+              Create New Project
+            </h1>
+            <form onSubmit={createProject} className="space-y-3">
+              <label className="text-black">Project name</label>
+              <input
+                type="text"
+                placeholder="Name"
+                className="w-full p-2 rounded-md bg-[#dadada]"
+                required
+              />
+            </form>
+            <div className="flex gap-2 justify-end">
+              <button className="bg-zinc-900  px-4 py-2 rounded-md w-[30%]">
+                Create
+              </button>
+              <button
+                className="bg- text-black px-4 py-2  border-black border-2 rounded-md w-[30%]"
+                onClick={() => {
+                  setIsModal(false);
+                }}
+              >
+                Cancel
+              </button>
             </div>
           </div>
         </div>
-
-        
-        <div className="flex items-center p-4 bg-[#212121] ">
-          <input
-            type="text"
-            placeholder="Ask me anything..."
-            className="w-full p-3 rounded-lg bg-[#2F2F2F]  focus:outline-none focus:ring-2 focus:ring-indigo0"
-          />
-          <button className="ml-3 p-2 bg-[#2F2F2F] text-white rounded-full hover:bg-slate-3">
-            <img className="opacity-30 w-6 " src={upload} alt="" />
-          
-          </button>
-        </div>
+      )}
+      <div className="projects p-4">
+        <button
+          className="p-4 rounded-md bg-[#eeee] text-black flex items-center gap-2"
+          onClick={() => {
+            setIsModal(true);
+          }}
+        >
+          <span className="font-semibold">Add Project</span>
+          {!isModal && <img src={add} className=" opacity0 w-5" />}
+        </button>
       </div>
-    </div>
+    </main>
   );
 };
 
