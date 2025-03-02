1e788b7975de8f62c26f4898bda1fe260dc8d21f
Hassan <syedhassan3038@gmail.com>
2025-03-02 17:37:23 +0500

    project card

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,13 +4,14 @@ import Home from "./pages/Home";
 import Login from './pages/Login'
 import Register from "./pages/Register";
 import { UserProvider } from "./context/UserContext";
+import UserAuth from "./auth/UserAuth";
 
 const App = () => {
   return (
     <div className="bg-zinc-800 text-neutral-300 min-h-screen min-w-screen">
       <UserProvider>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<UserAuth><Home /></UserAuth>} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
       </Routes>
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,23 +1,42 @@
-import React, { useContext, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { UserContext } from "../context/UserContext";
 import add from "../assets/add.svg";
 import axios from "../config/axios";
-
+import users from "../assets/user-3-fill.svg";
+import arrow from "../assets/information-line.svg";
 
 const Home = () => {
   const { user } = useContext(UserContext);
   const [isModal, setIsModal] = useState(false);
   const [projectName, setProjectName] = useState("");
-  
-  const createProject = (e)=>{
-    e.preventDefault()
-    console.log(projectName)
-    axios.post("/projects/create", {name:projectName}).then((res)=>{console.log(res.data)
-      setIsModal(false)
-      setProjectName("")
-    }).catch((err)=>{console.log(err)})
 
-  }
+  const [projects, setProjects] = useState([]);
+
+  const createProject = (e) => {
+    e.preventDefault();
+    console.log(projectName);
+    axios
+      .post("/projects/create", { name: projectName })
+      .then((res) => {
+        console.log(res.data);
+        setIsModal(false);
+        setProjectName("");
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  useEffect(() => {
+    axios
+      .get("/projects/all")
+      .then((res) => {
+        setProjects(res.data.projects);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, [isModal]);
 
   return (
     <main className="">
@@ -33,26 +52,30 @@ const Home = () => {
                 type="text"
                 placeholder="Name"
                 value={projectName}
-                onChange={(e)=>{setProjectName(e.target.value)}}
+                onChange={(e) => {
+                  setProjectName(e.target.value);
+                }}
                 className="w-full p-2 rounded-md bg-[#dadada] text-black"
                 required
               />
-               <div className="flex gap-2 justify-end">
-              <button type="submit" className="bg-zinc-900  px-4 py-2 rounded-md w-[30%]">
-                Create
-              </button>
-              <button
-              type="button"
-                className="bg- text-black px-4 py-2  border-black border-2 rounded-md w-[30%]"
-                onClick={() => {
-                  setIsModal(false);
-                }}
-              >
-                Cancel
-              </button>
-            </div>  
+              <div className="flex gap-2 justify-end">
+                <button
+                  type="submit"
+                  className="bg-zinc-900  px-4 py-2 rounded-md w-[30%]"
+                >
+                  Create
+                </button>
+                <button
+                  type="button"
+                  className="bg- text-black px-4 py-2  border-black border-2 rounded-md w-[30%]"
+                  onClick={() => {
+                    setIsModal(false);
+                  }}
+                >
+                  Cancel
+                </button>
+              </div>
             </form>
-           
           </div>
         </div>
       )}
@@ -66,6 +89,35 @@ const Home = () => {
           <span className="font-semibold">Add Project</span>
           {!isModal && <img src={add} className=" opacity0 w-5" />}
         </button>
+
+      <div className=" flex flex-wrap gap-4 mt-4">
+      {projects.map((project, index) => {
+          return (
+            <div
+              key={index}
+              className="min-w-[230px] p-6 bg-[#ffff] text-black border border-gray-200 rounded-lg   hover:shadow-neutral-600 hover:shadow-xl transition-all duration-500"
+            >
+              <div className=" flex flex-col justify-between ">
+                <h5 className="mb-2 text-lg font-medium tracking-tight">
+                  {project.name}
+                </h5>
+                <div className=" flex items-center gap-1">
+                  <img src={users} className="w-4" />
+                  <p className="text-sm">Collaborators: {project.users.length}</p>
+                </div>
+              </div>
+
+              <a
+                href="#"
+                className="inline-flex gap-2 items-center mt-2 px-3 py-2 text-sm font-medium text-center text-[#eeee] bg-black rounded-lg hover:bg-zinc-800 focus:ring-4 focus:outline-none focus:ring-zinc-300"
+              >
+                <p>Details</p>
+             <img src={arrow} className="w-4 invert" />
+              </a>
+            </div>
+          );
+        })}
+      </div>
       </div>
     </main>
   );
